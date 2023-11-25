91eba8468a048fb4ff284cc316231a3e8968ff4e
ligofff <liga@ligofff.ru>
2023-11-25 09:43:08 +0300

    String padding removed

--- Runtime/Logger.cs
+++ Runtime/Logger.cs
@@ -6,42 +6,42 @@ public static class Logger
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.Log($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}");
+        Debug.Log($"<color=#{color}>[{type.Name}]</color> {message}");
     }
 
     public static void Log(this UnityEngine.Object context, string message)
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.Log($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}", context);
+        Debug.Log($"<color=#{color}>[{type.Name}]</color> {message}", context);
     }
     
     public static void LogWarning(this object context, string message)
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.LogWarning($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}");
+        Debug.LogWarning($"<color=#{color}>[{type.Name}]</color> {message}");
     }
 
     public static void LogWarning(this UnityEngine.Object context, string message)
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.LogWarning($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}", context);
+        Debug.LogWarning($"<color=#{color}>[{type.Name}]</color> {message}", context);
     }
     
     public static void LogError(this object context, string message)
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.LogError($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}");
+        Debug.LogError($"<color=#{color}>[{type.Name}]</color> {message}");
     }
 
     public static void LogError(this UnityEngine.Object context, string message)
     {
         var type = context.GetType();
         var color = ColorUtility.ToHtmlStringRGB(GetColorFromObject(type));
-        Debug.LogError($"<color=#{color}>{$"[{type.Name}]",-35}</color> {message}", context);
+        Debug.LogError($"<color=#{color}>[{type.Name}]</color> {message}", context);
     }
     
     private static Color GetColorFromObject(object obj)
